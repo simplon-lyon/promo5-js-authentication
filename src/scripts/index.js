b4070f0bcdeb5f8612e23f70a2cc5947e677473e
matmorel <pub.imagiin@gmail.com>
2018-01-25 14:19:36 +0100

    add getUserByLogin() method + comments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,3 +12,8 @@ console.log(localStorage);
 model.addUser(mrToto);
 
 console.log(localStorage);
+// On récupère notre user fraichement créé.
+console.log(model.getUserByLogin(mrToto.email,mrToto.password));
+
+// Lorsqu'on n'a pas d'user correspondant à l'email et au mot de passe, on reçoit undefined.
+console.log(model.getUserByLogin("no@boby.com", "securepassword"));
--- src/scripts/user-model.js
+++ src/scripts/user-model.js
@@ -16,7 +16,7 @@ export class UserModel {
         //On va chercher le contenu actuel du localStorage sous la clef users
         let allUsers = localStorage.getItem("users");
         //Si il y a quelque chose à cette clef du localStorage
-        if(allUsers) {
+        if (allUsers) {
             //On le parse et on le met dans la variable users
             users = JSON.parse(allUsers);
         }
@@ -26,5 +26,24 @@ export class UserModel {
         //On stock dans le localStorage la nouvelle valeur du tableau
         //à la clef users
         localStorage.setItem("users", JSON.stringify(users));
+    }
+    /**
+     * La méthode getuserByLogin retourne un utilisateur à partir de ses infos de login.
+     * @param {String} email 
+     * @param {String} password 
+     */
+    getUserByLogin(email, password) {
+        // on récupère le contenu du localStorage puis on parse le JSON pour obtenir le tableau JS contenant les users
+        let userList = JSON.parse(localStorage.getItem('users'));
+        // pour chaque user...
+        for (let index = 0; index < userList.length; index++) {
+            // ... on vérifie que l'email et le mot de passe correspond à ceux passés à la méthode.
+            if (userList[index].email === email && userList[index].password === password) {
+                // si on trouve un user qui correspond à l'email et au mot de passe, on le retourne.
+                return userList[index];
+            }
+        }
+        // si aucun user n'a été trouvé, on retourne undefined
+        return undefined;
     }
 }
